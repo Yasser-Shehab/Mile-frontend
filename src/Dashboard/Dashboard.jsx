0a71fe635506f8afb1317d66b964720a4357db33
HishamAfifi <hish.abdelshafouk@gmail.com>
2022-03-03 14:09:27 -1000

    to be sure

--- src/Dashboard/Dashboard.jsx
+++ src/Dashboard/Dashboard.jsx
@@ -2,56 +2,57 @@ import { Menubar } from "primereact/menubar";
 import { InputText } from "primereact/inputtext";
 import NavBar from "../shared/NavBar/NavBar";
 import NavTap from "../shared/NavBar/NavTap";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 function Dashboard() {
+  let navigate = useNavigate();
   const items = [
     {
       label: "Accounts",
       icon: "pi pi-fw pi-file",
-      className: "p-menuitem",
+      command: () => {
+        navigate("/dashboard/accounts");
+      },
     },
     {
       label: "Costs",
       icon: "pi pi-fw pi-pencil",
+      command: () => {
+        navigate("/dashboard/costs");
+      },
     },
     {
       label: "Projects",
       icon: "pi pi-fw pi-user",
+      command: () => {
+        navigate("/dashboard/projects");
+      },
     },
     {
       label: "Specializations",
       icon: "pi pi-fw pi-calendar",
+      command: () => {
+        navigate("/dashboard/specializations");
+      },
     },
     {
       label: "Workers",
       icon: "pi pi-fw pi-power-off",
+      command: () => {
+        navigate("/dashboard/workers");
+      },
     },
   ];
 
   return (
-    <div>
-      <NavBar>
-        <NavTap path={`/dashboard/accounts`}>Accounts</NavTap>
-        <NavTap path={`/dashboard/costs`}>Costs</NavTap>
-        <NavTap path={`/dashboard/projects`}>Projects</NavTap>
-        <NavTap path={`/dashboard/specializations`}>Specializations</NavTap>
-        <NavTap path={`/dashboard/workers`}>Workers</NavTap>
-      </NavBar>
+    <>
       <div>
         <div className="card">
-          <Menubar
-            model={items}
-            start={
-              <NavTap className="p-menuitem" path={`/dashboard/accounts`}>
-                Accounts
-              </NavTap>
-            }
-          />
+          <Menubar model={items} />
         </div>
       </div>
 
       <Outlet />
-    </div>
+    </>
   );
 }
 
